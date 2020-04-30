e57b4cc88240b334c2080cf696346955ea1becef
yuta <9010atuy@gmail.copm>
2020-05-01 01:47:22 +0900

    First touch
    stateless widget first sample update

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -7,21 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Welcome to Flutter',
+        home: Scaffold(
+            appBar: AppBar(title: Text('Welcome to Flutter')),
+            body: Center(
+              child: Text('Hello World'),
+            )));
   }
 }
 
@@ -84,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
           // to see the wireframe for each widget.
           //
-          // Column has various properties to control how it sizes itself and
+          // C  olumn has various properties to control how it sizes itself and
           // how it positions its children. Here we use mainAxisAlignment to
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
@@ -92,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have clicked the button this many times:',
+              'You have the clicked the button this many times:',
             ),
             Text(
               '$_counter',
