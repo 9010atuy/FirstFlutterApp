098e7cb29fefbdb57ecf07f12e4d6522040bc11a
yuta <9010atuy@gmail.copm>
2020-05-06 12:57:35 +0900

    add statefulwidget, add infinite list
    build,,, add class

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+// import 'package:flutter/semantics.dart';
+// import 'package:flutter/semantics.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,12 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Welcome to Flutter',
-        home: Scaffold(
-            appBar: AppBar(title: Text('Welcome to Flutter')),
-            body: Center(
-              child: Text('Hello World'),
-            )));
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
   }
 }
 
@@ -99,4 +99,46 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Start up Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
